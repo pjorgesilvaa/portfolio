caa181ff201b57d1732acdeede86ce8fe511e893
Paulo Silva <pjorge.silvaa@gmail.com>
2025-03-22 17:19:58 +0000

    fix images

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio" : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "portfolio" : "",
-  },
 };
 
 export default nextConfig;
