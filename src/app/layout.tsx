22ef8f735b7b851524235c1b6c62e20f6dd8edf8
Paulo Silva <pjorge.silvaa@gmail.com>
2025-06-22 20:47:49 +0100

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-59X6K6K6LG" />
     </html>
   );
 }
